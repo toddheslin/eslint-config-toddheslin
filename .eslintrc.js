abc2a4e5c40967ef20a37887a99ef85f599bf4ba
toddheslin <toddheslin@users.noreply.github.com>
2020-05-10 11:51:18 +1200

    Removed root, line width, and avoid arrowParens

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,14 @@ module.exports = {
 		es6: true,
 		node: true
 	},
-	globals: {
-		__root: 'writable'
-	},
 	plugins: ['prettier', 'standard'],
 	extends: ['standard', 'prettier'],
 	rules: {
 		'prettier/prettier': ['error', {
-			trailingComma: 'es5',
-			singleQuote: true,
-			printWidth: 120,
-			semi: false
+      trailingComma: 'es5',
+      arrowParens: 'avoid',
+      singleQuote: true,
+      semi: false,
 		}]
 	}
 }
